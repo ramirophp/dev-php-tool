f6b364e7db17fd3042a18312f396bea179972dfb
rgg7888 <rgg7888@outlook.com>
2021-07-15 16:04:28 -0600

    head estructura forma 2

--- formas/f2.php
+++ formas/f2.php
@@ -13,6 +13,14 @@ XYZ::html('es');
 
         XYZ::meta(['charset' => 'UTF-8']);
 
+        XYZ::meta(['name' => 'description', 'content' => 'XYZ class php']);
+
+        XYZ::meta(['name' => 'keywords', 'content' => 'XYZ, class, php, html, css, javascript']);
+
+        XYZ::meta(['name' => 'author', 'content' => 'Ramiro Garcia Gonzalez']);
+
+        XYZ::meta(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1.0']);
+
     XYZ::_head();
 
 XYZ::_html();
