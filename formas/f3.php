6a5e353db5c640fb73fb2c01e07493121a86b70e
rgg7888 <rgg7888@outlook.com>
2021-07-15 20:33:48 -0600

    estructura forma 3

--- formas/f3.php
+++ formas/f3.php
@@ -1,7 +1,139 @@
 <?php
 
 require_once '../tool/forma_3.php';
+require_once '../tool/multiple_files/XYZ.php';
+
+$piezas = new Piezas;
 
 $evta = new Envoltura;
 
-$evta->html();
+define('ART','articulos');
+define('CAT','categorias');
+define('ETQ','etiquetas');
+define('ATR','atributos');
+
+$api = [ART,CAT,ETQ,ATR];
+
+XYZ::doctype();
+XYZ::html('es');
+
+    echo $evta->head([
+
+        $evta->title('forma 3'),
+        $evta->meta(['charset' => 'UTF-8']),
+        $evta->meta(['name' => 'description', 'content' => 'Envoltura class php']),
+        $evta->meta(['name' => 'keywords', 'content' => 'Envoltura, class, php, html, css, javascript']),
+        $evta->meta(['name' => 'author', 'content' => 'Ramiro Garcia Gonzalez']),
+        $evta->meta(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1.0'])
+
+    ]);
+
+    XYZ::body();
+
+        echo $evta->header($evta->h1('Piezas'));
+
+        echo $evta->nav($evta->ul([
+
+            $evta->a('Articulos',['href' => './f3.php?api=0']),
+            $evta->a('Categorias',['href' => './f3.php?api=1']),
+            $evta->a('Etiquetas',['href' => './f3.php?api=2']),
+            $evta->a('Atributos',['href' => './f3.php?api=3']),
+            $evta->a('Back',['href' => 'http://localhost'])
+
+        ]));
+
+        XYZ::main();
+
+            isset($_GET['api']) ? 
+
+                (    
+                    $api[$_GET['api']] == 'articulos' ? 
+                        
+                        var_dump($piezas->getArticles()).
+                        $evta->hr().$piezas->listArticles($piezas->getArticles())
+
+                    :
+
+                    (
+                        $api[$_GET['api']] == 'categorias' ? 
+
+                            var_dump($piezas->getCategories()).
+                            $evta->hr().$piezas->listCategories($piezas->getCategories())
+                        
+                        : 
+                        
+                        (
+                            $api[$_GET['api']] == 'etiquetas' ?
+
+                                var_dump($piezas->getTags()).
+                                $evta->hr().$piezas->listTags($piezas->getTags())
+                            
+                            :
+
+                            (
+                                $api[$_GET['api']] == 'atributos' ?
+
+                                    var_dump($piezas->getAttrs()).
+                                    $evta->hr().$piezas->listAttrs($piezas->getAttrs())
+
+                                :
+
+                                ''
+                            )
+                        )
+                    )
+                )
+
+            : '';
+
+        XYZ::_main();
+
+        echo $evta->section([
+
+            $evta->h2('Endpoints'),
+
+            $evta->ul([
+                "https://api.piezas.xyz/articulos/registros",
+                "https://api.piezas.xyz/articulos/registros/#",
+                "https://api.piezas.xyz/articulos/paginacion/#"
+            ]),
+
+            $evta->hr(),
+
+            $evta->ul([
+                "https://api.piezas.xyz/categorias/registros",
+                "https://api.piezas.xyz/categorias/registros/#",
+                "https://api.piezas.xyz/categorias/paginacion/#"
+            ]),
+
+            $evta->hr(),
+
+            $evta->ul([
+                "https://api.piezas.xyz/etiquetas/registros",
+                "https://api.piezas.xyz/etiquetas/registros/#",
+                "https://api.piezas.xyz/etiquetas/paginacion/#"
+            ]),
+
+            $evta->hr(),
+
+            $evta->ul([
+                "https://api.piezas.xyz/atributos/registros",
+                "https://api.piezas.xyz/atributos/registros/#",
+                "https://api.piezas.xyz/atributos/paginacion/#"
+            ])
+
+        ]);
+
+        echo $evta->footer([
+
+            $evta->h2('hello world.'),
+
+            $evta->div($evta->a('Ver Estructura - forma 3',[
+                'href' => 'https://github.com/ramirophp/dev-php-tool/blob/main/formas/f3.php'
+            ]))
+
+        ]);
+
+    XYZ::_body();
+
+XYZ::_html();
--- tool/multiple_files/Envoltura.php
+++ tool/multiple_files/Envoltura.php
@@ -35,4 +35,318 @@ class Envoltura {
 
     }
 
+    public function title($content) {
+
+        $tag = new Elemento([
+            'tag' => 'title',
+            'content' => $content
+        ]);
+
+        return $tag->getElement();
+
+    }
+
+    public function hr () {
+
+        $tag = new Elemento([
+            'tag' => 'hr',
+            'self' => true
+        ]);
+        
+        return $tag->getElement();
+
+    }
+
+    public function meta(array $attrs) {
+
+        $tag = new Elemento([
+            'tag' => 'meta',
+            'attrs' => $attrs,
+            'self' => true
+        ]);
+        
+        return $tag->getElement();
+
+    }
+
+    public function header ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'header',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'header',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function h1 ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'h1',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'h1',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function h2 ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'h2',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'h2',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function nav ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'nav',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'nav',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function ul (array $content = [],array $attrs = []) {
+
+        $items = [];
+
+        if (!empty($content)) {
+            for($i = 0; $i < count($content); $i++) {
+                $li = new Elemento([
+                    'tag' => 'li',
+                    'content' => $content[$i]
+                ]);
+                array_push($items,$li->getElement());
+            }
+        }
+
+        $content = [];
+        $content = $items;
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'ul',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'ul',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function a ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'a',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'a',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function main ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'main',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'main',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function section ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'section',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'section',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function footer ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'footer',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'footer',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function div ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'div',
+                'content' => $content
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'div',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
+    public function head ($content = '') {
+
+        $tag = new Elemento([
+            'tag' => 'head',
+            'content' => $content
+        ]);
+        
+        return $tag->getElement();
+
+    }
+
+    public function body ($content = '',array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'body',
+                'content' => $content,
+                'js' => true
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'body',
+                'attrs' => $attrs,
+                'content' => $content
+            ]);
+
+        }
+        
+        return $tag->getElement();
+
+    }
+
 }
