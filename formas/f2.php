e75da1f003570d3c4be224ed65e584c21d07df21
rgg7888 <rgg7888@outlook.com>
2021-07-15 18:52:38 -0600

    forma 2

--- formas/f2.php
+++ formas/f2.php
@@ -59,6 +59,122 @@ XYZ::html('es');
 
         XYZ::_nav();
 
+        XYZ::main();
+
+            $piezas = new Piezas;
+
+            define('ART','articulos');
+            define('CAT','categorias');
+            define('ETQ','etiquetas');
+            define('ATR','atributos');
+
+            $api = [ART,CAT,ETQ,ATR];
+
+            if (isset($_GET['api'])) {
+
+                switch ($api[$_GET['api']]) {
+
+                    case 'articulos' : 
+
+                        $articles = $piezas->getArticles();
+
+                        var_dump($articles);
+
+                        echo "<hr>";
+
+                        $piezas->listArticles($articles);
+
+                    break;
+
+                    case 'categorias' : 
+
+                        $categories = $piezas->getCategories();
+
+                        var_dump($categories);
+
+                        echo "<hr>";
+
+                        $piezas->listCategories($categories);
+
+                    break;
+
+                    case 'etiquetas' : 
+
+                        $tags = $piezas->getTags();
+
+                        var_dump($tags);
+
+                        echo "<hr>";
+
+                        $piezas->listTags($tags);
+
+                    break;
+
+                    case 'atributos' : 
+
+                        $attrs = $piezas->getAttrs();
+
+                        var_dump($attrs);
+
+                        echo "<hr>";
+
+                        $piezas->listAttrs($attrs);
+
+                    break;
+
+                }
+
+            }
+
+        XYZ::_main();
+
+        XYZ::section();
+
+            XYZ::h2();echo 'Endpoints';XYZ::_h2();
+
+            XYZ::ul();
+                XYZ::li();echo "https://api.piezas.xyz/articulos/registros";XYZ::_li();
+                XYZ::li();echo "https://api.piezas.xyz/articulos/registros/#";XYZ::_li();
+                XYZ::li();echo "https://api.piezas.xyz/articulos/paginacion/#";XYZ::_li();
+            XYZ::_ul();
+            XYZ::hr();
+
+            XYZ::ul();
+                XYZ::li();echo "https://api.piezas.xyz/categorias/registros";XYZ::_li();
+                XYZ::li();echo "https://api.piezas.xyz/categorias/registros/#";XYZ::_li();
+                XYZ::li();echo "https://api.piezas.xyz/categorias/paginacion/#";XYZ::_li();
+            XYZ::_ul();
+            XYZ::hr();
+
+            XYZ::ul();
+                XYZ::li();echo "https://api.piezas.xyz/etiquetas/registros";XYZ::_li();
+                XYZ::li();echo "https://api.piezas.xyz/etiquetas/registros/#";XYZ::_li();
+                XYZ::li();echo "https://api.piezas.xyz/etiquetas/paginacion/#";XYZ::_li();
+            XYZ::_ul();
+            XYZ::hr();
+
+            XYZ::ul();
+                XYZ::li();echo "https://api.piezas.xyz/atributos/registros";XYZ::_li();
+                XYZ::li();echo "https://api.piezas.xyz/atributos/registros/#";XYZ::_li();
+                XYZ::li();echo "https://api.piezas.xyz/atributos/paginacion/#";XYZ::_li();
+            XYZ::_ul();
+
+        XYZ::_section();
+
+        XYZ::footer();
+
+            XYZ::h2();echo "hello world.";XYZ::_h2();
+
+            XYZ::div();
+
+                XYZ::a(['href'=>'https://github.com/ramirophp/dev-php-tool/blob/main/formas/f2.php']);
+                    echo 'Ver Estructura - forma 2';
+                XYZ::_a();
+
+            XYZ::_div();
+
+        XYZ::_footer();
+
     XYZ::_body();
 
 XYZ::_html();
--- tool/multiple_files/XYZ.php
+++ tool/multiple_files/XYZ.php
@@ -14,6 +14,17 @@ class XYZ {
 
     }
 
+    public static function hr () {
+
+        $tag = new Elemento([
+            'tag' => 'hr',
+            'self' => true
+        ]);
+        
+        echo $tag->getElement();
+
+    }
+
     public static function html (string $lang) {
 
         $tag = new Elemento([
@@ -190,6 +201,39 @@ class XYZ {
 
     }
 
+    public static function h2 (array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'h2',
+                'self' => true
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'h2',
+                'attrs' => $attrs,
+                'self' => true
+            ]);
+
+        }
+        
+        echo $tag->getElement();
+
+    }
+
+    public static function _h2 () {
+
+        $tag = new Elemento([
+            'tag' => 'h2'
+        ]);
+        
+        echo $tag->close('h2');
+
+    }
+
     public static function nav (array $attrs = []) {
 
         if(empty($attrs)) {
@@ -319,6 +363,138 @@ class XYZ {
         ]);
         
         echo $tag->close('a');
+
+    }
+
+    public static function main (array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'main',
+                'self' => true
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'main',
+                'attrs' => $attrs,
+                'self' => true
+            ]);
+
+        }
+        
+        echo $tag->getElement();
+
+    }
+
+    public static function _main () {
+
+        $tag = new Elemento([
+            'tag' => 'main'
+        ]);
+        
+        echo $tag->close('main');
+
+    }
+
+    public static function section (array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'section',
+                'self' => true
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'section',
+                'attrs' => $attrs,
+                'self' => true
+            ]);
+
+        }
+        
+        echo $tag->getElement();
+
+    }
+
+    public static function _section () {
+
+        $tag = new Elemento([
+            'tag' => 'section'
+        ]);
+        
+        echo $tag->close('section');
+
+    }
+
+    public static function footer (array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'footer',
+                'self' => true
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'footer',
+                'attrs' => $attrs,
+                'self' => true
+            ]);
+
+        }
+        
+        echo $tag->getElement();
+
+    }
+
+    public static function _footer () {
+
+        $tag = new Elemento([
+            'tag' => 'footer'
+        ]);
+        
+        echo $tag->close('footer');
+
+    }
+
+    public static function div (array $attrs = []) {
+
+        if(empty($attrs)) {
+
+            $tag = new Elemento([
+                'tag' => 'div',
+                'self' => true
+            ]);
+
+        }else{
+
+            $tag = new Elemento([
+                'tag' => 'div',
+                'attrs' => $attrs,
+                'self' => true
+            ]);
+
+        }
+        
+        echo $tag->getElement();
+
+    }
+
+    public static function _div () {
+
+        $tag = new Elemento([
+            'tag' => 'div'
+        ]);
+        
+        echo $tag->close('div');
 
     }
 
